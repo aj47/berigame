e7aa22d9d7f209b9dd6a1701e81207c6e435f1f3
Arash Joobandi <arash@appricot.io>
2022-06-26 13:55:02 +1000

    new frontend

--- new_frontend/README.md
+++ new_frontend/README.md
@@ -1,1 +1,6 @@
-new front end
+# Notes Jun 24
+## Make alpha metaverse
+1. Plane (ground)
+2. Player Character / Controller
+3. Chat
+4. Environment
--- new_frontend/src/App.jsx
+++ new_frontend/src/App.jsx
@@ -8,6 +8,7 @@ import RenderGLB from "./Components/RenderGLB";
 import PropertiesPane from "./Panes/PropertiesPane";
 import { ChromePicker } from "react-color";
 import ChangeColorButton from "./Components/ChangeColorButton";
+import { DoubleSide, PlaneGeometry } from "three";
 
 // react three fiber docs
 // https://docs.pmnd.rs/react-three-fiber/api/objects
@@ -76,11 +77,18 @@ function App() {
       >
         <pointLight position={[10, 9, 10]} intensity={0.3} />
         <pointLight position={[-10, 10, -10]} intensity={0.3} />
-        <group position={[0, 2, 0]}>
+        {/* <group position={[0, 2, 0]}>
           <Suspense fallback={null}>
             <RenderGLB url={"./island.glb"} />
           </Suspense>
-        </group>
+        </group> */}
+        
+        {/* Ground Plane  */}
+        <mesh scale={[500, 500, 1]}  rotation={[Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+          <planeBufferGeometry/>
+          <meshBasicMaterial color="grey" side={DoubleSide}/>
+        </mesh>
+
       </ThreeJSCanvas>
     </>
   );
