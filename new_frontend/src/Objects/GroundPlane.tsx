9f7d040860ccd04fd4d2678ceb42e11fece6d091
Arash Joobandi <arash@appricot.io>
2022-07-15 19:46:40 +1000

    add island

--- new_frontend/src/Objects/GroundPlane.tsx
+++ new_frontend/src/Objects/GroundPlane.tsx
@@ -1,20 +1,29 @@
 import { useThree } from "@react-three/fiber";
-import React, { useRef } from "react";
+import React from "react";
 import { DoubleSide } from "three";
 
 const GroundPlane = (props) => {
-  const mesh = useRef() as any;
   return (
-    <mesh
-      ref={mesh}
-      name="land_mesh"
-      scale={[500, 500, 1]}
-      rotation={[Math.PI / 2, 0, 0]}
-      position={[0, -0.1, 0]}
-    >
-      <planeBufferGeometry />
-      <meshBasicMaterial color="#006994" side={DoubleSide} />
-    </mesh>
+    <>
+      <mesh
+        name="land_mesh"
+        scale={[50, 50, 1]}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, -0, 0]}
+      >
+        <planeBufferGeometry />
+        <meshBasicMaterial color="#fff1a1" side={DoubleSide} />
+      </mesh>
+      <mesh
+        name="water_mesh"
+        scale={[500, 500, 1]}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, -0.1, 0]}
+      >
+        <planeBufferGeometry />
+        <meshBasicMaterial color="#006994" side={DoubleSide} />
+      </mesh>
+    </>
   );
 };
 
