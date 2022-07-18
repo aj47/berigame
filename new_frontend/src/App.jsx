1950b1a4054fb135550bf18587d471694cf6694b
Arash Joobandi <arash@appricot.io>
2022-07-18 22:21:16 +1000

    loading

--- new_frontend/src/App.jsx
+++ new_frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
 import ThreeJsCanvas from "./Panes/ThreeJSCanvas";
-import { Magic } from 'magic-sdk';
+import { Magic } from "magic-sdk";
 
 function App() {
   const magic = new Magic(import.meta.env.VITE_MAGIC_API_KEY);
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const tryAuth = async () => {
     try {
       await magic.auth.loginWithCredential();
@@ -20,23 +22,28 @@ function App() {
   const checkAuth = async () => {
     const isLoggedIn = await magic.user.isLoggedIn();
     if (isLoggedIn) {
+      if (window.location.pathname !== "/play")
+        window.location.href = window.location.origin + "/play";
       const userMetadata = await magic.user.getMetadata();
       setUserData(userMetadata);
-      if (window.location.pathname !== "/play")
-      window.location.href = window.location.origin + "/play";
+      setLoading(false);
     } else if (window.location.pathname !== "/") {
       window.location.href = window.location.origin;
     }
+    setLoading(false);
   };
 
   useEffect(() => {
     if (window.location.pathname === "/login-callback") tryAuth();
-    else if(userData === null) checkAuth();
+    else if (userData === null) checkAuth();
   }, [userData]);
 
-  return <>
-    <p className="username-text">user: {userData.email}</p>
-  {userData ? <ThreeJsCanvas /> : <Login />}</>;
+  return (
+    <>
+      {/* <p className="username-text">user: {userData.email}</p> */}
+      {loading ? "Loading..." : userData ? <ThreeJsCanvas /> : <Login />}
+    </>
+  );
 }
 
 export default App;
