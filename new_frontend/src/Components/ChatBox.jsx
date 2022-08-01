058c251b6af65f2d4e6a775ce2ebdcf009ea9ca7
Arash Joobandi <arash@appricot.io>
2022-08-01 21:05:31 +1000

    enter to bring up chat

--- new_frontend/src/Components/ChatBox.jsx
+++ new_frontend/src/Components/ChatBox.jsx
@@ -1,16 +1,52 @@
-import { ReactComponentElement, ReactElement, useState } from "react";
-import { setAppendChatLog, webSocketConnect, webSocketConnection, webSocketSendMessage } from "../Api";
-const ChatBox = ({setChatMessageSent}) => {
+import {
+  ReactComponentElement,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
+import {
+  setAppendChatLog,
+  webSocketConnect,
+  webSocketConnection,
+  webSocketSendMessage,
+} from "../Api";
+const ChatBox = ({ setChatMessageSent }) => {
   const [chatOpen, setChatOpen] = useState(false);
+  const [inputText, setInputText] = useState("");
   webSocketConnect();
 
+  const enterPressed = () => {
+    if (!chatOpen) setChatOpen(true);
+    if (inputText !== "") sendMessage();
+    if (chatOpen) setChatOpen(false);
+  };
+
+  const sendMessage = () => {
+    webSocketSendMessage(inputText);
+    setChatMessageSent(inputText);
+    setTimeout(() => {
+      setChatMessageSent(null);
+    }, 6000);
+    setInputText("");
+  };
+
+  useEffect(() => {
+    const keyDownHandler = (e) => {
+      if (e.code === "Enter") {
+        e.preventDefault();
+        enterPressed();
+      }
+    };
+    document.addEventListener("keydown", keyDownHandler);
+  });
+
   const ChatLog = () => {
-    const [chatLogArray, setChatLogArray] = useState([])
+    const [chatLogArray, setChatLogArray] = useState([]);
     const appendChatLogArray = (data) => {
       console.log(data, "dat");
       setChatLogArray([...chatLogArray, data]);
       console.log(chatLogArray, "chatLogArray");
-    }
+    };
     setAppendChatLog(appendChatLogArray);
     return (
       <div
@@ -19,8 +55,11 @@ const ChatBox = ({setChatMessageSent}) => {
       >
         {chatLogArray.map((data, i) => {
           return (
-            <div key={i} >
-              <p><strong>Player said: </strong>{data.message}</p>
+            <div key={i}>
+              <p>
+                <strong>Player said: </strong>
+                {data.message}
+              </p>
             </div>
           );
         })}
@@ -28,34 +67,25 @@ const ChatBox = ({setChatMessageSent}) => {
     );
   };
 
-  const ChatInputBar = () => {
-    const [inputText, setInputText] = useState("");
-    return (
-      <div className="chatInputBar">
-        <textarea
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-        <button
-          onClick={(e) => {
-            webSocketSendMessage(inputText);
-            setChatMessageSent(inputText);
-            setTimeout(() => {
-              setChatMessageSent(null);
-            }, 3000);
-            setInputText("");
-          }}
-        >
-          Send
-        </button>
-      </div>
-    );
-  };
-
   return (
     <div id="chatBox">
       <ChatLog />
-      {chatOpen && <ChatInputBar />}
+      {chatOpen && (
+        <div className="chatInputBar">
+          <textarea
+            autoFocus={true}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <button
+            onClick={(e) => {
+              sendMessage();
+            }}
+          >
+            Send
+          </button>
+        </div>
+      )}
       <button
         className="openChatButton"
         onClick={(e) => {
