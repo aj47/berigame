9f7aa0fb913cdcd17c219cda8c7edfe06f77c301
Arash Joobandi <arash@appricot.io>
2022-08-19 15:40:51 +1000

    cubespaced alpha v1.1

--- new_frontend/src/App.jsx
+++ new_frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Logout from "./Components/Logout";
 import GameComponent from "./Components/3D/GameComponent";
 
 function App() {
+  console.log("cubespaced alpha v1.1");
   const [userData, setUserData] = useState(null);
 
   return (
