1433ce826981a11f7380494309dc4a4bbe66d3a1
Eric-Chen1990 <842436130@qq.com>
2023-02-11 18:29:46 +0800

    fix camera controller

--- src/Components/3D/CameraController.tsx
+++ src/Components/3D/CameraController.tsx
@@ -1,29 +1,21 @@
-import { useFrame, useThree } from "@react-three/fiber";
-import React, { useEffect, useMemo } from "react"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
+import { CameraControls } from "@react-three/drei";
 
 const CameraController = (props) => {
-  const { camera , gl } = useThree();
-  const controls = useMemo(() => new OrbitControls(camera, gl.domElement), [])
-  useEffect(() => {
-    controls.minDistance = 10;
-    controls.maxDistance = 10;
-    // controls.maxPolarAngle = Math.PI/2 - 0.1; 
-    camera.position.set(0, 2, 8);
-    controls.update();
-    return () => {
-      controls.dispose();
-    };
-  }, [camera, gl]);
-  
-  useFrame(() => {
-    if (!props.playerRef?.current?.position) return
-    const {x,y,z} = props.playerRef?.current?.position;
-    controls.target.set(x,y,z);
-    return controls.update();
-  })
+	const ref = useRef<CameraControls | null>(null);
+	useEffect(() => {
+		ref.current?.setFocalOffset(0, -2, 4);
+	}, []);
 
-  return null;
+	useFrame(() => {
+		if (props.playerRef?.current?.position) {
+			const { x, y, z } = props.playerRef?.current?.position;
+			ref.current?.moveTo(x, y, z, true);
+		}
+	});
+
+	return <CameraControls ref={ref} minDistance={10} />;
 };
 
 export default CameraController;
