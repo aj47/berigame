9bd0cbd392d3826548b0c4d0f2df8f835a7e77f4
AJ <arash@pragma.com.au>
2023-02-16 15:25:46 +1100

    dropdown always in view

--- src/Components/ClickDropdown.tsx
+++ src/Components/ClickDropdown.tsx
@@ -5,15 +5,20 @@ const ClickDropdown = (props) => {
   const clickedOtherObject = useUserInputStore(
     (state: any) => state.clickedOtherObject
   );
-  useEffect(() => {
-    console.log(clickedOtherObject);
-  });
+  const clickedX = clickedOtherObject.e.clientX;
+  const clickedY = clickedOtherObject.e.clientY;
+  // Used to offset dropdown so it is always in view
+  const translateVal =
+    (clickedX > window.innerWidth / 2 ? "-100%" : "0") +
+    " , " +
+    (clickedY > window.innerHeight / 2 ? "-100%" : "0");
   return (
     <div
       className="click-dropdown"
       style={{
         top: clickedOtherObject.e.clientY,
         left: clickedOtherObject.e.clientX,
+        transform: `translate(${translateVal})`,
       }}
     >
       ---
