c19abcd251d8a3ac9e3647fffe01cabbfd1c3061
Arash Joobandi <arash@appricot.io>
2023-02-23 23:07:39 +1100

    show dropdown on tree click

--- newFE/src/Components/3D/BerryTree.tsx
+++ newFE/src/Components/3D/BerryTree.tsx
@@ -10,13 +10,33 @@ const BerryTree = (props) => {
   const onClick = (e) => {
     e.stopPropagation();
     // materialChange();
-    setClickedOtherObject({ ...objRef, isCombatable, connectionId, e });
+    setClickedOtherObject({
+      ...objRef,
+      isCombatable: false,
+      connectionId: "TREE",
+      e,
+      dropdownOptions: [
+        {
+          label: "Harvest",
+          onClick: () => {
+            console.log("test");
+          },
+        },
+      ],
+    });
     // setTimeout(() => {
     //   clearMaterialChange();
     // }, 150);
   };
 
-  return <RenderGLB objRef={objRef} url={"/tree.glb"} position={[5, 0, 0]} />;
+  return (
+    <RenderGLB
+      onClick={onClick}
+      objRef={objRef}
+      url={"/tree.glb"}
+      position={[5, 0, 0]}
+    />
+  );
 };
 
 export default BerryTree;
