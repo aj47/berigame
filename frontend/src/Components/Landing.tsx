75f127e90cbd6006105bedd6136475d3f2750dff
Joscar <joscar.n@outlook.com>
2023-03-26 18:18:12 +1100

    basic landing modal and place at top level

---
+++ frontend/src/Components/Landing.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to Beri Island</h1>
+      <ul>
+        <li>
+          <a href="#">High Scores</a>
+        </li>
+        <li>
+          <a href="#">Discord</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Landing;
